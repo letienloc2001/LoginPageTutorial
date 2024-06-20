1fc7a298525e8b6ae49ad9c3989be234bd0427a3
letienloc2001 <letienloc2001@gmail.com>
2024-06-20 17:24:23 +0700

    second commit

---
+++ LoginPageTutorial/Models/MyDBContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace LoginPageTutorial.Models
+{
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext(DbContextOptions<MyDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<User> Users { get; set; }
+
+    }
+}
--- LoginPageTutorial/Program.cs
+++ LoginPageTutorial/Program.cs
@@ -1,8 +1,11 @@
+using LoginPageTutorial.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
+builder.Services.AddDbContext<MyDbContext>(e => e.UseSqlServer(builder.Configuration.GetConnectionString("DBCS")));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
