afa1ba3ee57c8f135e8d36c3818c7506f55bf7d0
letienloc2001 <letienloc2001@gmail.com>
2024-06-20 17:07:22 +0700

    Add project files.

---
+++ LoginPageTutorial/Models/User.cs
@@ -0,0 +1,15 @@
+﻿namespace LoginPageTutorial.Models
+{
+    public class User
+    {
+        public int UserId { get; set; } = 0;
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public int isActive { get; set; } = 1;
+        public DateTime CreatedOn { get; set; } = DateTime.Now;
+
+
+    }
+}
