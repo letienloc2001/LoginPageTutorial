fca1bfc5c5ceaf1479ace20783d757394c105694
letienloc2001 <letienloc2001@gmail.com>
2024-06-26 14:40:26 +0700

    init

--- LoginPageTutorial/wwwroot/js/site.js
+++ LoginPageTutorial/wwwroot/js/site.js
@@ -2,3 +2,7 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+{/* <script 
+    src="https://kit.fontawesome.com/64d58efce2.js"
+    crossOrigin="anonymous">
+</script> */}
